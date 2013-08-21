29849e5413ba1b4a47d6a8cd8b42f05ca56f84e9
Tony Salamanca <ikibits@gmail.com>
2013-08-21 14:43:01 -0500

    Se inicializo las tablas

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,3 +15,36 @@
 #ENV['ADMIN_PASSWORD'].dup
 #puts 'user: ' << user.name
 #user.add_role :admin
+require 'database_cleaner'
+
+DatabaseCleaner.strategy = :truncation
+
+# then, whenever you need to clean the DB
+DatabaseCleaner.clean
+
+#puts 'ROLES'
+#YAML.load(ENV['ROLES']).each do |role|
+#  Role.find_or_create_by_name(role)
+#  puts 'role: ' << role
+#end
+#user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
+
+puts 'DEFAULT USERS'
+user = User.find_or_create_by_email :name => "Tony Salamanca Ilaquita", :email => "bunseki.bits@gmail.com", :password => "Shuuryoo<suru2ni.wa", :password_confirmation => "Shuuryoo<suru2ni.wa"
+puts 'user: ' << user.name
+user.add_role :admin
+
+Region.create(nombre: "Tacna")
+Region.create(nombre: "Moquegua")
+Region.create(nombre: "Puno")
+Region.create(nombre: "Arequipa")
+Region.create(nombre: "Arica")
+
+Institucion.create(nombre: "UNJBG")
+Institucion.create(nombre: "UPT")
+Institucion.create(nombre: "UAP")
+Institucion.create(nombre: "ULC")
+Institucion.create(nombre: "FPGV")
+
+Estudio.create(nombre:"Estudiante")
+Estudio.create(nombre: "Profesional")
