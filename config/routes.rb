22a14c4a5db7be4b37acfc26b0774579378889cd
Tony Salamanca <ikibits@gmail.com>
2013-08-22 00:45:50 -0500

    Se agrego la ruta conferencia

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 HackDay::Application.routes.draw do
   
+  get "conferencia/index"
   get "cronogramas/index"
   root :to => "home#index"
   devise_for :users, :controllers => {:registrations => "registrations"}
