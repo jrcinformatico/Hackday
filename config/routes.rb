f5d7833623e5953c119eb424464f3ca99198e9b5
Tony Salamanca <ikibits@gmail.com>
2013-08-24 12:45:19 -0500

    Se link nuevamente a conferencias y eventos

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 HackDay::Application.routes.draw do
   
   get "reportes/index"
-  #get "conferencia/index"
-  #get "cronogramas/index"
+  get "conferencia/index"
+  get "cronogramas/index"
   root :to => "home#index"
   devise_for :users, :controllers => {:registrations => "registrations"}
   resources :users
