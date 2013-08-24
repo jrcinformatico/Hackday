dffcecffc548c69cdf18128e45d803a521eca648
Tony Salamanca <ikibits@gmail.com>
2013-08-24 11:56:15 -0500

    Se comento el campo dni

--- app/models/persona.rb
+++ app/models/persona.rb
@@ -1,14 +1,16 @@
 class Persona < ActiveRecord::Base
 	belongs_to :region
-	belongs_to :institucion
+	#belongs_to :institucion
 	belongs_to :estudio
 	
+	has_and_belongs_to_many :eventos
+		
 	validates :nombres, :apellidos, :email, :estudio_id, :presence => true
 	
-	validates_uniqueness_of :dni
+	#validates_uniqueness_of :dni
 	validates_uniqueness_of :email
-	validates :dni,length: { is: 8}, :presence => true
-	validates_format_of :dni, :with => /\A[0-9]{8}\Z/
+	#validates :dni,length: { is: 8}, :presence => true
+	#validates_format_of :dni, :with => /\A[0-9]{8}\Z/
 	
 	validates_uniqueness_of :celular
 	validates :celular,length: { is: 9}, :presence => true
