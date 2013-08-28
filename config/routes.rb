1e214324a716853405748624e2ac9bc316e4e9b8
Tony Salamanca <ikibits@gmail.com>
2013-08-27 22:57:01 -0500

    Se establece link de conferencias(index, ponencias, talleres,costos)

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 HackDay::Application.routes.draw do
   
-  get "reportes/index"
-  #get "conferencia/index"
-  #get "cronogramas/index"
+  get "conferencias/index"
+  get "conferencias/ponencias"
+  get "conferencias/talleres"
+  get "conferencias/costos"
+  get "reportes/index"  
+  get "cronogramas/index"
   root :to => "home#index"
   devise_for :users, :controllers => {:registrations => "registrations"}
   resources :users
